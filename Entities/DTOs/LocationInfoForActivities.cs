9b8058cd32a8b560ef0bb8706f780454d2fdbe67
ardaberrun <ardaberrun@gmail.com>
2022-12-26 20:35:30 +0300

    fix LocationInfoForActivities

--- Entities/DTOs/LocationInfoForActivities.cs
+++ Entities/DTOs/LocationInfoForActivities.cs
@@ -9,6 +9,8 @@ namespace Entities.DTOs
     public class LocationInfoForActivities
     {
         public int Id { get; set; }
+        public int CityId { get; set; }
+        public int CountryId { get; set; }
         public string Name { get; set; }
         public string CityName { get; set; }
         public string CountryName { get; set; }
